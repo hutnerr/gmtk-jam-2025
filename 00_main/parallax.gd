61f79f012a2194c16c279d0609033d55a386a39d
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-07-30 19:17:28 -0400

    more parallax tweaks

--- 00_main/parallax.gd
+++ 00_main/parallax.gd
@@ -1,6 +1,6 @@
 extends ParallaxBackground
 
-var scroll_speed := Vector2(30, 0) # Adjust as needed
+var scroll_speed := Vector2(30, 0)
 
 func _process(delta: float) -> void:
 	scroll_offset += scroll_speed * delta
