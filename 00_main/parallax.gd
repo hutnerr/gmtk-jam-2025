eb5287c1a57fc1dff612de1eeff9517a92817de9
Jakob Bennie <jayfeather874@gmail.com>
2025-07-30 21:54:03 -0400

    More art

--- 00_main/parallax.gd
+++ 00_main/parallax.gd
@@ -1,6 +1,6 @@
 extends ParallaxBackground
 
-var scroll_speed := Vector2(30, 0)
+@export var scrollSpeed: Vector2 = Vector2(30, 0)
 
 func _process(delta: float) -> void:
-	scroll_offset += scroll_speed * delta
+	scroll_offset += scrollSpeed * delta
