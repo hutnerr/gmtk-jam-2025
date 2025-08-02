29832738d6d5f27ae6c8c9035fb9366957259d87
Jakob Bennie <jayfeather874@gmail.com>
2025-08-02 14:33:28 -0400

    better art

--- 03_objects/wall/wall.gd
+++ 03_objects/wall/wall.gd
@@ -5,10 +5,10 @@ extends GridObject
 @onready var animSprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	#if not animName:
-		#var animList = animSprite.sprite_frames.get_animation_names()
-		#animName = animList[randi_range(0, len(animList) - 1)]
-		#pass
+	if not animName:
+		var animList = animSprite.sprite_frames.get_animation_names()
+		animName = animList[randi_range(0, len(animList) - 1)]
+		pass
 	type = GridObject.ObjectType.WALL
 	animSprite.play(animName)
 
