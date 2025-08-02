bf494df6ca2c05fa6ebd71ee3b283c3537f5ae69
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-08-02 16:06:43 -0400

    what imu doing

--- 01_user-interface/how-to-play/how_to_play.gd
+++ 01_user-interface/how-to-play/how_to_play.gd
@@ -3,8 +3,8 @@ extends Control
 const MAIN_MENU = "res://00_main/Main.tscn"
 const LEVEL_ONE = "res://05_levels/Level1.tscn"
 
-@onready var goodBoyButton: Button = $VBoxContainer/MarginContainer/VBoxContainer/GoodBoyButton
-@onready var levelOneButton: Button = $VBoxContainer/MarginContainer/VBoxContainer/LevelOneButton
+@onready var goodBoyButton: Button = $VBoxContainer/HBoxContainer/MarginContainer2/VBoxContainer/MarginContainer/VBoxContainer/GoodBoyButton
+@onready var levelOneButton: Button = $VBoxContainer/HBoxContainer/MarginContainer2/VBoxContainer/MarginContainer/VBoxContainer/LevelOneButton
 
 func _ready() -> void:
 	goodBoyButton.pressed.connect(onGoodBoyButtonPressed)
