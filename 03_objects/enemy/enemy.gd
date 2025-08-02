b8fb7d6421299a011aba84bd00be9de7f14b9575
Jakob Bennie <jayfeather874@gmail.com>
2025-08-02 12:40:11 -0400

    Enemies Die Now

--- 03_objects/enemy/enemy.gd
+++ 03_objects/enemy/enemy.gd
@@ -1,4 +1,17 @@
 extends GridObject
 
+
+
+@onready var hitbox: Area2D = $Area2D
+
 func _ready() -> void:
 	type = GridObject.ObjectType.ENEMY
+	hitbox.area_entered.connect(onAreaEntered)
+	
+	
+
+
+
+func onAreaEntered(area):
+	if self.is_visible_in_tree():
+		self.visible = false
