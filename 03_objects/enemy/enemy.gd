99acd3ef608d8a7963d3b72c8c4c390656281e9f
Jakob Bennie <jayfeather874@gmail.com>
2025-08-03 10:39:28 -0400

    Centaur

--- 03_objects/enemy/enemy.gd
+++ 03_objects/enemy/enemy.gd
@@ -1,19 +1,29 @@
 extends GridObject
 
 
-@export var enemySprite: String
 @onready var hitbox: Area2D = $Area2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var monkeyAnimRandomization: Array[String] = ["Idle", "Idle", "Idle", "Idle", "Idle", "Stomp", "Stab"]
 
 func _ready() -> void:
 	type = GridObject.ObjectType.ENEMY
 	hitbox.area_entered.connect(onAreaEntered)
-	if enemySprite:
-		sprite.play(enemySprite)
-
+	sprite.animation_finished.connect(onAnimFinished)
+	
+	
 # Should play some sort of death animation, then connect that to this invisibility thing
 func onAreaEntered(area):
 	if self.is_visible_in_tree():
-		if self.visible:
-			AudiManny.playEnemyDeadSFX()
-		self.visible = false
+		sprite.stop()
+		sprite.play("Death")
+		AudiManny.playEnemyDeadSFX()
+		await sprite.animation_finished
+		killMyBoi()
+
+func killMyBoi() -> void:
+	self.visible = false
+	
+func onAnimFinished() -> void:
+	sprite.play(monkeyAnimRandomization.pick_random())
+	
