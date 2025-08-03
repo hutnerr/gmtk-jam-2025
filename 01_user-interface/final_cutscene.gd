e077c3399f3f0a39364355fc224015e9c7d6178f
Jakob Bennie <jayfeather874@gmail.com>
2025-08-03 11:51:38 -0400

    Finished cutscene

--- 01_user-interface/final_cutscene.gd
+++ 01_user-interface/final_cutscene.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 @onready var animPlayer = $AnimationPlayer
-
+@onready var mainGuitarLoop = 	$"Guitar-mainloop"
 func _ready() -> void:
 	animPlayer.animation_finished.connect(onDonePlaying)
+	mainGuitarLoop.finished.connect(replay)
 
 func onDonePlaying(something) -> void:
+	mainGuitarLoop.play()
 	print("roll the fuckin credits baby")
 	pass
+
+func stfu() -> void:
+	mainGuitarLoop.stop()
+
+func replay() -> void:
+	mainGuitarLoop.play()
